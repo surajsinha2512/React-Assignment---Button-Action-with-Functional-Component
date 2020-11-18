de1d121b93cd2c9dd8170d89947bb2f86e9a36ad
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-11-19 03:22:32 +0530

    added para

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [showPara,setShowPara]=useState(false)
   return (
-    <div id="main">
-      // Do not alter the main div
+    <div id="main"> 
+     {showPara?<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>:null}
+     <button id="click" onClick={()=>{setShowPara(true)}}>click here</button>
     </div>
   );
 }
